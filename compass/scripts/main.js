c4505d7c3d94f71966abca660f4fdce7e1c8e7cd
Puspam Adak <puspam12345678@outlook.com>
2023-10-03 03:18:30 +0530

    Modularized and added styles

---
+++ compass/scripts/main.js
@@ -0,0 +1,40 @@
+import Dialog from "./dialog.js";
+
+window.applyDarkTheme = () => {
+	document.body.setAttribute("data-theme", "dark");
+	theme_btn.children[0].setAttribute("file", "light");
+}
+window.applyLightTheme = () => {
+	document.body.setAttribute("data-theme", "light");
+	theme_btn.children[0].setAttribute("file", "dark");
+}
+window.toggleTheme = () => {
+	document.body.getAttribute("data-theme") == "dark" ? applyLightTheme() : applyDarkTheme();
+}
+
+const infoDialog = new Dialog(info_dialog);
+infoDialog.cancellable = true;
+
+window.showInfo = () => {
+	pauseMeasure();
+	infoDialog.open();
+	if (info_dialog.dataset.loaded) return;
+	fetch("info.html")
+		.then(response => response.text())
+		.then(text => {
+			info_dialog.children[0].remove();
+			info_dialog.innerHTML = text;
+		});
+}
+window.hideInfo = () => {
+	infoDialog.close();
+	startMeasure();
+}
+
+var preferred_theme = localStorage.getItem("compass-theme");
+preferred_theme === "dark" ? applyDarkTheme() : applyLightTheme();
+
+window.addEventListener("beforeunload", () => {
+	let current_theme = document.body.getAttribute("data-theme");
+	if (current_theme !== preferred_theme) localStorage.setItem("compass-theme", current_theme);
+});
